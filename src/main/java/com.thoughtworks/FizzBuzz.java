2c1a29376bde6cac705c7b3b64e68c554f3e60da
lw <623656827@qq.com>
2020-06-15 15:00:22 +0800

    重构357

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -12,25 +12,40 @@ public class FizzBuzz {
         boolean isDividedBy3 = i % 3 == 0;
         boolean isDividedBy5 = i % 5 == 0;
         boolean isDividedBy7 = i % 7 == 0;
-        if (isDividedBy3 && isDividedBy5 && isDividedBy7) {
-            return "FizzBuzzWhizz";
+//        if (isDividedBy3 && isDividedBy5 && isDividedBy7) {
+//            return "FizzBuzzWhizz";
+//        }
+//        if (isDividedBy3 && isDividedBy7) {
+//            return "FizzWhizz";
+//        }
+//        if (isDividedBy3 && isDividedBy5) {
+//            return "FizzBuzz";
+//        }
+//
+//        if (isDividedBy3) {
+//            return "Fizz";
+//        }
+//        if (isDividedBy5) {
+//            return "Buzz";
+//        }
+//        if (isDividedBy7) {
+//            return "Whizz";
+//        }
+//        return String.valueOf(i);
+        StringBuilder result = new StringBuilder();
+        if(!(isDividedBy3||isDividedBy5||isDividedBy7)){
+            return String.valueOf(i);
         }
-        if (isDividedBy3 && isDividedBy7) {
-            return "FizzWhizz";
-        }
-        if (isDividedBy3 && isDividedBy5) {
-            return "FizzBuzz";
-        }
-
         if (isDividedBy3) {
-            return "Fizz";
+            result.append("Fizz");
         }
         if (isDividedBy5) {
-            return "Buzz";
+            result.append("Buzz");
         }
         if (isDividedBy7) {
-            return "Whizz";
+            result.append("Whizz");
         }
-        return String.valueOf(i);
+        return result.toString();
     }
+
 }
