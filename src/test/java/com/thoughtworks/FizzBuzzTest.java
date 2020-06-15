25c5435654f499875edbf83ad0802063d3d82321
lw <623656827@qq.com>
2020-06-15 13:33:46 +0800

    11

---
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class FizzBuzzTest {
+    @Test
+    public void hello_world_test() {
+        //given
+        Dependency dependency = new Dependency();
+        HelloWorld helloWorld = new HelloWorld(dependency);
+
+        //when
+        String actual = helloWorld.beenCalled();
+
+        //then
+        assertEquals(actual,"Leave me alone.");
+        assertThat(actual,is("1"));
+    }
+}
