354e427127260f1a2e60f49cf118b0b51b987a71
lw <623656827@qq.com>
2020-06-15 13:52:30 +0800

    should_return_fizzBuzz_given_15

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,12 +1,21 @@
 package com.thoughtworks;
 
+/**
+ * FizzBuzz
+ *
+ * @author louwei
+ * @date 2020/6/15
+ */
 public class FizzBuzz {
 
     public String say(int i) {
-        if(i%3==0){
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (i % 3 == 0) {
             return "Fizz";
         }
-        if(i%5==0){
+        if (i % 5 == 0) {
             return "Buzz";
         }
         return String.valueOf(i);
